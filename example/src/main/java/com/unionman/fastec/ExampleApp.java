171f8cceebcf2bf7280d5eb3f54b8881ddc8d511
yuanhao <yuanhao.zhang@unionman.com.cn>
2017-11-09 22:47:49 +0800

    完善了下载请求和调试拦截器的代码

--- example/src/main/java/com/unionman/fastec/ExampleApp.java
+++ example/src/main/java/com/unionman/fastec/ExampleApp.java
@@ -19,7 +19,7 @@ public class ExampleApp extends Application{
         Latte.init(this)
                 .withIcon(new FontAwesomeModule())
                 .withIcon(new FontEcModule())
-                .withApiHost("http://127.0.0.1/")
+                //.withApiHost("http://127.0.0.1/")
                 .withInterceptor(new DebugInterceptor("index",R.raw.text))
                 .configure();
 
